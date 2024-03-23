f4519c129777d728fb616e481db57c154f4e95df
Tono507 <maxantonio507@gmail.com>
2024-03-23 05:24:42 +0100

    update 1.1

--- component4.js
+++ component4.js
@@ -1,21 +1,21 @@
 AFRAME.registerComponent('hello', {
-    schema: {
-        message: {type: 'string', default: 'Hello!'},
-        event: {type: 'string', default: ''},
-      },
+  schema: {
+      message: {type: 'string', default: 'Hello!'},
+      event: {type: 'string', default: ''},
+    },
 
-    init: function () {
-      var data = this.data;  // Component property values.
-      var el = this.el;  // esto apunta al componente en el que estoy usando este componente
-  
-      if (data.event) {
-        // This will log the `message` when the entity emits the `event`.
-        el.addEventListener(data.event, function () {
-          console.log(data.message);
-        });
-      } else {
-        // `event` not specified, just log the message.
-        console.log(data.message);
-      };
-    }
-  });
+  init: function () {
+    var data = this.data;  // Component property values.
+    var el = this.el;  // Reference to the component's entity.
+
+    if (data.event) {
+      // This will log the `message` when the entity emits the `event`.
+      el.addEventListener(data.event, function () {
+        el.setAttribute('visible', false);
+      });
+    } else {
+      // `event` not specified, just log the message.
+      console.log(data.message);
+    };
+  }
+});
